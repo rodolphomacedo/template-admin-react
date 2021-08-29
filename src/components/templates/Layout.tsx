07429e802a4fb9e1b00280f51bc1ce391a809561
rodolphomacedo <rodolpho.ime@gmail.com>
2021-08-29 17:12:03 -0300

    Enable darkmode in tailwindcss config

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -11,17 +11,17 @@ interface LayoutProps{
 
 export default function Layout(props: LayoutProps) {
     return (
-        <div className={`
-            flex flex-col h-screen 
-            justify-center items-center
-            bg-gradient-to-br from-red-200 to-blue-200
-            
-        `}>
-            <TopBar title={ props.title } subtitle={ props.subtitle }/>
+        <div className={`flex h-screen w-screen`}>
             <SideBar />
-            <Content>
-                { props.children }
-            </Content>
+            <div className={`
+                    flex flex-col 
+                    w-full p-7
+                    bg-gray-300 dark:bg-gray-800`}>
+                <TopBar title={ props.title } subtitle={ props.subtitle }/>
+                <Content>
+                    { props.children }
+                </Content>
+            </div>
         </div>
     )
 }
--- src/components/templates/SideBar.tsx
+++ src/components/templates/SideBar.tsx
@@ -5,7 +5,7 @@ interface SideBarProps{
 export default function SideBar(props) {
     return (
         <aside>
-            <h1></h1>
+            <h1>Side Bar</h1>
         </aside>
     )
 }
