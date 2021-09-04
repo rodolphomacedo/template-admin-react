0f89405646c7d5cd1cad5cb4a70aa65186275d04
rodolphomacedo <rodolpho.ime@gmail.com>
2021-09-04 11:55:09 -0300

    Start build Login\SignUp page and components

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,21 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
     return (
-        <div>
-            <label>{ props.label }</label>
+        <div className={`mt-5`}>
+            <label className={`text-gray-400`}>
+                { props.label }:
+            </label>
             <input 
                 type={ props.type ?? 'text'} 
                 value={ props.value } 
                 onChange={ e => props.toggleValue?.(e.target.value) }
                 required={ props.required }
+                className={` 
+                    px-4 py-3 rounded-full bg-gray-100 ml-3
+                    border focus:border-blue-300 focus:outline-none
+                    focus:bg-gray-200 focus:text-indigo-500
+                    text-gray-400
+                `}
             />
         </div>
     )
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -6,25 +6,57 @@ export default function Auth() {
     const [email, setEmail] = useState('')
     const [password, setPassword ] = useState('')
 
-    return (
-        <div>
-            <h1>Autenticação</h1>
-    
-            <AuthInput 
-                label="E-mail"
-                type="email"
-                value={ email }
-                toggleValue={setEmail}
-                required
-            />
+    function submit(){
+        if (mode === 'login') {
+            console.log('login')
+        } else {
+            console.log('cadastrar')
+        }
+    }
 
-            <AuthInput 
-                label="Senha"
-                type="password"
-                value={ password }
-                toggleValue={setPassword}
-                required
-            />
+    return (
+        <div className={` 
+            flex flex-columns h-screen 
+            justify-center items-center
+        `}>
+            <div className={`w-1/2`}>
+                <h1 className={`
+                    text-xl font-bold mb-5 
+                `}>
+                    {mode === 'login' ? 'Entre com sua Conta' : 'Cadastre-se na Plataforma'}
+                </h1>
+        
+                <AuthInput 
+                    label="E-mail"
+                    type="email"
+                    value={ email }
+                    toggleValue={setEmail}
+                    required
+                />
+
+                <AuthInput 
+                    label="Senha"
+                    type="password"
+                    value={ password }
+                    toggleValue={setPassword}
+                    required
+                />
+                <button onClick={submit} className={`
+                    w-full bg-indigo-500 hover:bg-indigo-600 
+                    text-white rounded-lg px-4 py-3 mt-6
+                `}>
+                    { mode === 'login' ? 'Entrar' : 'Cadastrar' }
+                </button>
+
+                <hr className=" my-6 border-gray-100 w-full"/>
+                
+                <button onClick={submit} className={`
+                    w-full bg-red-500 hover:bg-red-600
+                    text-white rounded-lg px-4 py-3
+                `}>
+                    Entrar com Google
+                </button>
+            </div>
         </div>
     )
 }
