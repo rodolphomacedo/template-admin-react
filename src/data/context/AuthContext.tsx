f21040988b74ee377ed8db3e84bfa6f7b89c5289
rodolphomacedo <rodolpho.ime@gmail.com>
2021-09-14 10:45:51 -0300

    Add Session inside cookie to Auth Users

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import  route  from 'next/router'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
 import firebase from '../../firebase/config'
 import UserModel from '../../model/UserModel'
 
@@ -11,32 +12,62 @@ interface AuthContextProps {
 const authContext = createContext<AuthContextProps>({})
 
 async function userNormalized(userFirebase: firebase.User): Promise<UserModel> {
-    const token = await userFirebase.getIdToken()
+    
+    const token = await userFirebase.getIdToken()   
+
     return {
         uid: userFirebase.uid,
         name: userFirebase.displayName,
         email: userFirebase.email,
-        token, 
+        token: token, 
         provider: userFirebase.providerData[0].providerId,
         photoUrl: userFirebase.photoURL
     }
 }
 
+function manageCookie(logged: boolean) {
+    if (logged) {
+        Cookies.set('admin-template-cod3r-auth', logged, {
+            expires: 7
+        })
+    } else {
+        Cookies.remove('admin-template-cod3r-auth')
+    }
+}
+
 export function AuthProvider(props) {
 
+    const [loading, setLoading] = useState(true)
     const [user, setUser] = useState<UserModel>(null)
+
+    async function setupSession(userFirebase) {
+        const user = await userNormalized(userFirebase)
+        if (userFirebase?.email) {
+            setUser(user)
+            manageCookie(true)
+            setLoading(false)
+            return user.email
+        } else {
+            setUser(null)
+            manageCookie(false)
+            setLoading(false)
+            return false
+        }
+    }
 
     async function loginGoogle() {
         const resp = await firebase.auth().signInWithPopup(
             new firebase.auth.GoogleAuthProvider()
         )
 
-        if(resp.user?.email){
-            const userLogged = await userNormalized(resp.user)
-            setUser(userLogged)
-            route.push('/')
-        }
+        setupSession(resp.user)
+        route.push('/')
     }
+
+    useEffect(() => {
+        const cancel = firebase.auth().onIdTokenChanged(setupSession)
+        return () => cancel()
+    },  [])
 
     return (
         <authContext.Provider value={{
